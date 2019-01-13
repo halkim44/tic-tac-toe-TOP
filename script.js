e26eb127f9cde9cf63634f24960bbcccb97455c8
Halkim44 <kimketchum15@gmail.com>
2019-01-13 22:13:08 +0700

    interface cleanup and unbeatable CPU AI added

--- script.js
+++ script.js
@@ -5,35 +5,31 @@ document.addEventListener("keypress", function (e) {
   let oName = document.querySelector('#p-name-o');
 
   if (e.key === 'Enter') {
-    if (!/^\S+$/.test(xName.value) || !/^\S+$/.test(oName.value)) {
+    if (!/^\S+$/.test(xName.value) || !/^\S+$/.test(oName.value) || xName.value === oName.value) {
       return "";
     }
     playerX = Player(xName.value, "X");
     playerO = Player(oName.value, "O");
-    document.querySelector('.name-query-window').remove();
 
-    if(!playerX.isHuman) {
-      Gameboard.addMark(playerX.inhumanMove(Gameboard.getGrid()), GameControll.getTurn());
-      GameControll.switchTurn();
-    }
+    document.querySelector('.main-container').classList.add('game-start');
+
+    GameControll.playerXStart(Gameboard, playerX);
 
     document.querySelectorAll('.item').forEach((node, i) => {
       node.addEventListener('click', function () {
-        Gameboard.addMark(i, GameControll.getTurn());
-    
-        if (GameControll.checkIfStrike(Gameboard.getGrid())) {
-          GameControll.getTurn() ? playerX.win() : playerO.win();
-          Gameboard.clearBoard();
-    
-        } else if (Gameboard.checkIfAllAreMarked()) {
-          alert('draw');
-          Gameboard.clearBoard();
+        if (Gameboard.addMark(i, GameControll.getTurn())) {
+
+          GameControll.check(Gameboard, playerX, playerO);
+
+          let nextPlayer = GameControll.getTurn() ? playerX : playerO;
+          if (typeof nextPlayer.inhumanMove === "function") {
+            Gameboard.addMark(nextPlayer.inhumanMove(Gameboard.getGrid(), GameControll), GameControll.getTurn());
+            GameControll.check(Gameboard, playerX, playerO);
+          }
         }
-    
-        GameControll.switchTurn();
       })
     })
-  
+
   } else {
     setTimeout(function () {
       if (/^\S+$/.test(xName.value) && !/^\S+$/.test(oName.value)) {
@@ -53,10 +49,12 @@ const Gameboard = (() => {
   ];
 
   function addMark(i, isPlayerOne) {
-    if (grid[i] == "") {
-      grid[i] = isPlayerOne ? "X" : "O";
-      render();
+    if (grid[i] !== "") {
+      return false;
     }
+    grid[i] = isPlayerOne ? "X" : "O";
+    render();
+    return true;
   }
 
   function render() {
@@ -70,7 +68,7 @@ const Gameboard = (() => {
     })
   }
 
-  function clearBoard() {
+  function clear() {
     grid = grid.map(function (el) {
       return el = "";
     });
@@ -89,7 +87,7 @@ const Gameboard = (() => {
 
   return {
     addMark,
-    clearBoard,
+    clear,
     getGrid,
     checkIfAllAreMarked
   }
@@ -125,9 +123,44 @@ const GameControll = (() => {
     })
   }
 
+  function check(board, pX, pO) {
+    let winner;
+    if (checkIfStrike(board.getGrid())) {
+      winner = getTurn() ? pX : pO;
+      window.setTimeout(function () {
+        winner.win();
+        board.clear();
+      }, 1);
+
+    } else if (board.checkIfAllAreMarked()) {
+      winner = {};
+      window.setTimeout(function () {
+        alert('draw');
+        board.clear();
+      }, 1);
+    }
+
+    if (typeof winner !== "object") {
+      switchTurn();
+    } else {
+      window.setTimeout(function () {
+        isPlayerXturn = true;
+        GameControll.playerXStart(board, pX);
+      }, 2);
+    }
+  }
+
+  function playerXStart(board, pX) {
+    if (typeof pX.inhumanMove === "function") {
+      board.addMark(pX.randomMove(board.getGrid()), getTurn());
+      switchTurn();
+    }
+  }
   return {
     getTurn,
     switchTurn,
+    check,
+    playerXStart,
     checkIfStrike
   }
 })();
@@ -136,23 +169,96 @@ const Player = (name, xo) => {
   let score = 0;
   let playerObj = {};
 
-    document.querySelector(`#player-${xo}-name`).textContent = name;
+  document.querySelector(`#player-${xo}-info > .name`).textContent = name;
 
   playerObj.win = function () {
     alert(name + " win!");
     score++;
-    document.querySelector(`#player-${xo}-score`).textContent = score;
+    document.querySelector(`#player-${xo}-info .score`).textContent = score;
   }
-  playerObj.isHuman = true;
-  if(name === "CPU") {
-    playerObj.isHuman = false;
-    playerObj.inhumanMove = function(grid) {
-      let pos;
 
-      do {
-        pos = Math.floor(Math.random() * 9);
-      } while(grid[pos] !== "");
-      return pos;
+  playerObj.X = xo;
+  if (name === "CPU") {
+
+    let nextMove;
+    //only if X is the first to start
+    playerObj.randomMove = function (grid) {
+      while (true) {
+        let index = Math.floor(Math.random() * grid.length);
+        if (grid[index] == "") {
+          return index;
+        }
+      }
+    }
+
+    function determineMaximizingPlayer(numOfEmptySpace) {
+      if (numOfEmptySpace % 2 == 0) {
+        return "O";
+      } else {
+        return "X";
+      }
+    }
+
+    function minimax(gameState, availableOpt, game) {
+
+      if (availableOpt.length == 0 || game.checkIfStrike(gameState)) {
+        if (!game.checkIfStrike(gameState)) {
+          return 0;
+        } else {
+          if (determineMaximizingPlayer(availableOpt.length) !== xo) {
+            return availableOpt.length + 1;
+          } else {
+            return availableOpt.length - 10;
+
+          }
+        }
+      }
+
+      let values = [];
+
+      availableOpt.forEach(function (option, i, arr) {
+        arr = arr.slice();
+        arr.splice(i, 1);
+        let newGameState = gameState.slice();
+        newGameState[option] = determineMaximizingPlayer(availableOpt.length);
+        values.push(minimax(newGameState, arr, game));
+      })
+
+      let idealValue;
+      nextMoves = availableOpt[0];
+      if(determineMaximizingPlayer(availableOpt.length) === xo) {
+        idealValue = -Infinity;
+        for(let i = 0; i < values.length; i++) {
+          if(values[i] >= idealValue) {
+            idealValue = values[i];
+            nextMove = availableOpt[i];
+          }
+        }
+      } else {
+        idealValue = Infinity;
+        for(let i = 0; i < values.length; i++) {
+          if(values[i] <= idealValue) {
+            idealValue = values[i];
+            nextMove = availableOpt[i];
+          }
+        }
+      }
+
+      return idealValue;
+    }
+
+    playerObj.inhumanMove = function (grid, game) {
+      let emptySquares = [];
+
+      for (let i = 0; i < grid.length; i++) {
+        if (grid[i] == "") {
+          emptySquares.push(i);
+        }
+      }
+
+      minimax(grid, emptySquares, game);
+      console.log(nextMove);
+      return nextMove;
     }
   }
   return playerObj;
